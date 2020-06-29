87e59b6926cfae01a8dc6f29e35d6b7722fccdb0
Munna Singh <munna.singh@techjini.com>
2020-06-29 21:17:41 +0530

    added string & viewwillappear method

--- SonarDemo/SonarDemo/ViewController.swift
+++ SonarDemo/SonarDemo/ViewController.swift
@@ -9,12 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var nameName: String? = ""
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
 
 }
 
